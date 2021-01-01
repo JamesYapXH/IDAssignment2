2c01b4137e19d8dd214d3a95840a731b133c21db
Liquid2K <jamesyapxuheng@gmail.com>
2021-01-01 21:48:02 +0800

    added javascript, to change between man and women calories and added login form

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-document.addEventListener("click",calculateCalories)
+const btn = document.querySelector('#btn');
+        // handle click button
+        btn.onclick = function () {
+            const rbs = document.querySelectorAll('input[name="gender"]');
+            let selectedValue;
+            for (const rb of rbs) {
+                if (rb.checked) {
+                    selectedValue = rb.value;
+                    break;
+                }
+            }
+            if(selectedValue == "male"){
+                document.getElementById("calogoal").innerHTML = "Your calorie goal: 2300 calories"
+            }
 
-function calculateCalories(){
-    var x = document.forms["submit_form"]["caloriesconsumed"].value;
-
-}
-
+           else if(selectedValue == "female"){
+                document.getElementById("calogoal").innerHTML = "Your calorie goal: 1900 calories"
+           }
+                
+            
+        };
