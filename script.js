945199fa82ced5b0ee13f67637f0a60d15a0b1ec
Liquid2K <jamesyapxuheng@gmail.com>
2021-01-09 15:26:56 +0800

    +reset calorie count at 12 am

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+function scheduleReset() {
+    // get current time
+    let reset = new Date();
+    // update the Hours, mins, secs to the 24th hour (which is when the next day starts)
+    reset.setHours(24, 0, 0, 0);
+    // calc amount of time until restart
+    let t = reset.getTime() - Date.now();
+    setTimeout(function() {
+        // reset variable
+        if(userObj.gender == "male"){
+            userObj.calories = 2300;
+            let saveObj = JSON.stringify(userObj);
+            sessionStorage.setItem("currentUser",saveObj);
+            localStorage.setItem(userObj.username,saveObj);
+        }
+        else if(userObj.gender == "female"){
+            userObj.calories = 1900;
+            let saveObj = JSON.stringify(userObj);
+            sessionStorage.setItem("currentUser",saveObj);
+            localStorage.setItem(userObj.username,saveObj);
+        }
+        // schedule the next variable reset
+        scheduleReset();
+    }, t);
+}
+
 if(sessionStorage.getItem("loggedIn")== "true"){
     let userObj = JSON.parse(sessionStorage.getItem("currentUser"));
     console.log(userObj);
+    scheduleReset();
 
     $(".calorienum").text(userObj.calories);
     console.log(userObj.calories);
@@ -9,10 +36,7 @@ if(sessionStorage.getItem("loggedIn")== "true"){
         btn.onclick = function () {
             console.log("test");
             let userInput = document.getElementById("caloriesConsumed").value;
-            console.log(userObj.calories);
-            console.log(userInput);
             let newValue = Number(userObj.calories) - Number(userInput);
-            console.log(newValue);
             userObj.calories = newValue;
             let saveObj = JSON.stringify(userObj);
             sessionStorage.setItem("currentUser",saveObj);
